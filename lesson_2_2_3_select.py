41bc7f67772ee0fe3fa5f25ec0d51dc662f64b0c
Serg-Klimenko <klimenko.sm.job@gmail.com>
2021-01-12 19:40:08 +0200

    task 2.2.3 select

---
+++ lesson_2_2_3_select.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import time
+
+link = "http://suninjuly.github.io/selects1.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    num1 = browser.find_element_by_id("num1").text
+    num2 = browser.find_element_by_id("num2").text
+    sum = int(num1) + int(num2)
+    browser.find_element_by_tag_name("select").click()
+    browser.find_element_by_css_selector(f"[value='{sum}'").click()
+    browser.find_element_by_css_selector("button.btn").click()
+finally:
+    time.sleep(30)
+    browser.quit()
