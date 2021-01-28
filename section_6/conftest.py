388355c1059c4c7f68f9544625129dee0d434232
Serg-Klimenko <klimenko.sm.job@gmail.com>
2021-01-28 21:25:34 +0200

    task 3.6.6 test attribute in Cl for conftest

--- section_6/conftest.py
+++ section_6/conftest.py
@@ -1,13 +1,27 @@
 import pytest
 from selenium import webdriver
 
+
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store", default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    # disable ChromeDevTools's logging on display
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    browser = webdriver.Chrome(options=options)
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        # disable ChromeDevTools's logging on display
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     # this part will be execute after test was finished
     print("\nquit browser..")
