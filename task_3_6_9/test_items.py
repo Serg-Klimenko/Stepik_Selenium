7a55a2106978672670dfa2fe4dd6aea0513b7ee4
Serg-Klimenko <klimenko.sm.job@gmail.com>
2021-01-29 19:02:02 +0200

    task 3.6.9 test different languages

--- task_3_6_9/test_items.py
+++ task_3_6_9/test_items.py
@@ -7,7 +7,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_button_basket_on_spanish_language(browser):
     browser.get(link)
-    time.sleep(30)
-    # test for spanish language only
-    button_text = browser.find_element_by_css_selector("button.btn-add-to-basket").text
-    assert button_text == "Añadir al carrito"
+    time.sleep(10)
+    button = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert button is not None, "Button 'Add to basket' is not present"
