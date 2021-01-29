fbaf41a03cacf5dc8cb097ca5423b4246462fd39
Serg-Klimenko <klimenko.sm.job@gmail.com>
2021-01-29 19:13:00 +0200

    task 3.6.9 test different languages

--- task_3_6_9/test_items.py
+++ task_3_6_9/test_items.py
@@ -5,7 +5,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_button_basket_on_spanish_language(browser):
+def test_guest_should_see_button_basket(browser):
     browser.get(link)
     time.sleep(10)
     button = browser.find_element_by_css_selector("button.btn-add-to-basket")
